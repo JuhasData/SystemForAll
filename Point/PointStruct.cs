31619dfa09a3299591a94e8aa53e298525a91633
Mantyla <juha.p.mantyla@cgi.com>
2017-08-22 13:42:57 +0800

    Change Framework

--- Point/PointStruct.cs
+++ Point/PointStruct.cs
@@ -8,20 +8,60 @@ namespace SystemForAll.Point
 {
     public struct PointStruct<T>
     {
-        // Generic state date.
+        //Add User information
+        private User user;
+        private Session session;
+        private Location location;
+        private Global global;
+        private Entity entity;
+
+                // Generic state date.
         private T xPos;
         private T yPos;
         private T zPos;
 
         // Generic constructor.
-        public PointStruct(T xVal, T yVal, T zVal)
+        public PointStruct(T xVal, T yVal, T zVal, User userVal, Session sessionVal, Location locationVal, Global globalVal, Entity entityVal)
         {
             xPos = xVal;
             yPos = yVal;
             zPos = zVal;
+            user = userVal;
+            session = sessionVal;
+            location = locationVal;
+            global = globalVal;
+            entity = entityVal;
         }
 
         // Generic properties.
+        public User userProp 
+        { 
+           get { return user; }
+           set { user = value; } 
+        }
+
+        public Session sessionProp 
+        {
+             get {return session; }
+             set { session = value;} 
+        }
+        public Location locationProp 
+        { 
+            get { return location; }
+            set; { location = value; }
+        }
+        public Global globalProp 
+        { 
+            get { return global; }
+            set { global = value; }
+        }
+
+        public Entity entityProp 
+        { 
+            get { return entity;} 
+            set { entity = value;} 
+        }
+
         public T X
         {
             get { return xPos; }
@@ -43,7 +83,7 @@ namespace SystemForAll.Point
 
         public override string ToString()
         {
-            return string.Format("[{0}, {1}, {2}]", xPos, yPos, zPos);
+            return string.Format("[{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}]", xPos, yPos, zPos, user, session, location, global, entity);
         }
 
         // Reset fields to the default value of the
@@ -53,6 +93,11 @@ namespace SystemForAll.Point
             xPos = default(T);
             yPos = default(T);
             zPos = default(T);
+            user = default(null);
+            session = default(null)
+            location = default(null);
+            global = default(null);
+            entity = default(null);
         }
     }
 }
